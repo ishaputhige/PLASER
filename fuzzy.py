3177c3987bcb447c16efa86fe90c16a07015d5b5
Suneet Jain <suneet.bhopal@gmail.com>
2021-10-12 20:34:35 +0530

    Updated fuzzy.py

--- fuzzy.py
+++ fuzzy.py
@@ -1,7 +1,7 @@
-from getpass import getpass
-from mysql.connector import connect, Error
-from fuzzywuzzy import process
 def get_details(arr):
+    from getpass import getpass
+    from mysql.connector import connect, Error
+    from fuzzywuzzy import process
     names_list = []
     try:
         with connect(
@@ -51,10 +51,16 @@ def get_details(arr):
                             row = row.replace("\n"," ")
                             dic[highest[0]] = [rating,row]
                     else:
-                        # dic[name] = ["NA","Unable to find"]
+                        if len(name) > 22:
+                            pass
+                        else:
+                            dic[name] = ["NA","Unable to find"]
                         # print("\n")
-                        continue
         except Error as e:
             print(e)
-    return dic
+    print(dic)
 
+
+
+arr = ['Aqua', 'Glycerin', 'Stearic Acid', 'Lauric Acid', 'Myristic Acid', 'Potassium Laureth Phosphate', 'Potassium a Hydroxide', 'Palmitic Acid', 'Ceteareth25', 'Sodium Cocoyl oo Glycinate', 'Glycol Distearate', 'Hydrogenated Castor Oil', 'an Tremella Fuciformis Sporocarp Extract', 'Dendrobium is Candidum Extract', 'Sodium Taurine Laurate', 'a ee Polyquatemium7', 'Charcoal Powder', 'Com Starch ot Modified', 'Sodium Hydroxymethyiglycinate', 'Hydroxypropy  Methyicellulose', 'Disodium EDTA', 'Tocopherol', 'Acetyi i Glucosamine', 'Aroma etcreger Store t Ke ealty placer Avold oy cunight en yhigh temperature Sy _ a  Manufacture Date SODMMYYYYAS shown on the package Sam oo Expiration Date DDMMIYYYY As shown on the package aaa  cage ta RS Ns cmemekiflpuiner tal onercemecmatieee Pe  CEES LSD Guangdong Essence Dally ily Chemical a Co ances OKETEY Road Shenshan Administrative Bia Area J langgao Ja0', 'Town eaiyun Distric i Guangzhou City SaaCne  Guangdon ng Province China oe aa fiiow to Users Apply appropriate amount onto wet skir io eg Med ayay  Cael   gay massage inva a circular movement rinse off be  CutioniFor oxternal use joniysAvold contact ran ha Ss  SUE uct gets Into eyes MNT withw wale Sr a sential call  mage a Immediately Piease keep it out of reach of children', 'ry qhsonuit guse if signs of irritation orrash appeals oe om', 'allowed seek medical assistance iis enibe Planiaciae Tor SRTRe Scr Fabtcado para NO IM ye']
+get_details(arr)
