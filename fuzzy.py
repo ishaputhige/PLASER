6f743948972b92730b78ca6c66c5eb1acac1ebf7
Suneet Jain <suneet.bhopal@gmail.com>
2021-11-11 14:31:23 +0530

    Updated fuzzy.py

--- fuzzy.py
+++ fuzzy.py
@@ -40,6 +40,7 @@ def get_details(arr):
                     if highest:
                         select_movies_query = "SELECT column4,column5 FROM cosmetic_ingredients where column1='%s'" % (highest[0])
                         cursor.execute(select_movies_query)
+                        
                         result = cursor.fetchall()
                         for rating,row in result:
                             # name = 
@@ -58,9 +59,5 @@ def get_details(arr):
                         # print("\n")
         except Error as e:
             print(e)
-    print(dic)
-
-
-
-arr = ['Aqua', 'Glycerin', 'Stearic Acid', 'Lauric Acid', 'Myristic Acid', 'Potassium Laureth Phosphate', 'Potassium a Hydroxide', 'Palmitic Acid', 'Ceteareth25', 'Sodium Cocoyl oo Glycinate', 'Glycol Distearate', 'Hydrogenated Castor Oil', 'an Tremella Fuciformis Sporocarp Extract', 'Dendrobium is Candidum Extract', 'Sodium Taurine Laurate', 'a ee Polyquatemium7', 'Charcoal Powder', 'Com Starch ot Modified', 'Sodium Hydroxymethyiglycinate', 'Hydroxypropy  Methyicellulose', 'Disodium EDTA', 'Tocopherol', 'Acetyi i Glucosamine', 'Aroma etcreger Store t Ke ealty placer Avold oy cunight en yhigh temperature Sy _ a  Manufacture Date SODMMYYYYAS shown on the package Sam oo Expiration Date DDMMIYYYY As shown on the package aaa  cage ta RS Ns cmemekiflpuiner tal onercemecmatieee Pe  CEES LSD Guangdong Essence Dally ily Chemical a Co ances OKETEY Road Shenshan Administrative Bia Area J langgao Ja0', 'Town eaiyun Distric i Guangzhou City SaaCne  Guangdon ng Province China oe aa fiiow to Users Apply appropriate amount onto wet skir io eg Med ayay  Cael   gay massage inva a circular movement rinse off be  CutioniFor oxternal use joniysAvold contact ran ha Ss  SUE uct gets Into eyes MNT withw wale Sr a sential call  mage a Immediately Piease keep it out of reach of children', 'ry qhsonuit guse if signs of irritation orrash appeals oe om', 'allowed seek medical assistance iis enibe Planiaciae Tor SRTRe Scr Fabtcado para NO IM ye']
-get_details(arr)
+    print(dic.keys())
+    return dic
